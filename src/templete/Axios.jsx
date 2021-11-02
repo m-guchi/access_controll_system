12996c01c2025827b9e52084ff02828ce1f9fbab
guchimina -thinkpad <kazu9sum.mi@gmail.com>
2021-11-02 18:08:21 +0900

    back api version1

--- src/templete/Axios.jsx
+++ src/templete/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const customAxios = axios.create({
-    baseURL: process.env.REACT_APP_API_URL + "/v2",
+    baseURL: process.env.REACT_APP_API_URL + "/v1",
     withCredentials: true,
     validateStatus: function (status) {
         return status < 500;
