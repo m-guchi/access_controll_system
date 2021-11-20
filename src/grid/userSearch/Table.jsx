c1ff375fea7f30eb9085ae39642671b7c6e2fcec
guchimina -desktop <kazu9sum.mi@gmail.com>
2021-11-21 02:53:00 +0900

    ユーザー検索

--- src/grid/userSearch/Table.jsx
+++ src/grid/userSearch/Table.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { DataGrid, GridToolbar, GridOverlay } from '@material-ui/data-grid';
 
@@ -8,28 +7,6 @@ import { DataGrid, GridToolbar, GridOverlay } from '@material-ui/data-grid';
 function escapeRegExp(value) {
     return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        padding: theme.spacing(0.5, 0.5, 0),
-        justifyContent: 'space-between',
-        display: 'flex',
-        alignItems: 'flex-start',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        [theme.breakpoints.down('xs')]: {
-          width: '100%',
-        },
-        margin: theme.spacing(1, 0.5, 1.5),
-        '& .MuiSvgIcon-root': {
-          marginRight: theme.spacing(0.5),
-        },
-        '& .MuiInput-underline:before': {
-          borderBottom: `1px solid ${theme.palette.divider}`,
-        },
-    },
-}));
 
 function CustomLoadingOverlay() {
     return (
@@ -46,8 +23,6 @@ const columns = [
 ]
 
 export default function UserSearchTable (props) {
-
-    const contextInfo = props.infoData
 
     const row = [];
     if(props.userList){
